b78a295bd3163104a7272fd3a7d858bbaca6540d
mooooon333 <742932097@qq.com>
2020-06-22 19:33:56 +0800

    “重命名”

--- TodoMVC.js
+++ TodoMVC.js
@@ -1,35 +1,26 @@
-const CL_COMPLETED = "completed";
+
 let taskId = 1; //use guid
 var items = [];
 function $(id) {
-	return document.getElementById(id);
+	return document.querySelector(id);
 }
 
 window.onload = function () {
 	model.init();
 	
-	$("todo").addEventListener("keyup", function (event) {
+	$("#todo").addEventListener("keyup", function (event) {
 		if (event.keyCode != 13) return;
-		addTodo();
+		addTask();
 	});
-	// 这里赋值const是没有用的，后边用不了
-	// const taskList = document.querySelector(".tasks-list__list");
-	// const finishedTaskList = document.querySelector(
-	// 	".finished-tasks-list__list"
-	// );
-	const finishedHeader = document.querySelector(".finished-header");
-
-
-	// taskList.addEventListener("click", clickTask);
+	const finishedHeader = $(".finished-header");
 	finishedHeader.addEventListener("click", toggleFinished);
-    // finishedTaskList.addEventListener("click", clickTask);
     this.update();
 };
 function toggleFinished() {
-    const finishedHeader = document.querySelector(".finished-header");
+    const finishedHeader = $(".finished-header");
 	const chevronIcon = finishedHeader.querySelector("i");
 	if (chevronIcon.classList.contains("finished-list-closed")) {
-		// console.log("yes");
+
 		chevronIcon.classList.replace(
 			"finished-list-closed",
 			"finished-list-open"
@@ -40,8 +31,8 @@ function toggleFinished() {
 			"finished-list-closed"
 		);
     }
-    const finishedTaskList = document.querySelector(
-		".finished-tasks-list__list"
+    const finishedTaskList = $(
+		".finished-tasks-list"
 	);
 	finishedTaskList.classList.toggle("hidden");
 }
@@ -51,88 +42,53 @@ function update() {
 	model.flush();
 	var data = model.data;
 
-	// var list = $("list");
-	// list.innerHTML = "";
-	// for (var i = 0; i < data.items.length; ++i) {
-	// 	(function (index) {
-	// 		var it = data.items[index];
-	// 		createTodo(it);
-	// 	})(i);
-	// }
 
 	var activeCount = 0;
 	data.items.forEach(function (item) {
-		if (!item.completed) ++activeCount;
+		if (!item.finished) ++activeCount;
 	});
-    $("count").innerHTML = activeCount + " items left";
+    $("#count").innerHTML = activeCount + " items left";
     
-
-	var taskStore = data.items;
-	taskList = document.querySelector(".tasks-list__list");
-	finishedTaskList = document.querySelector(".finished-tasks-list__list");
+	taskList = $(".tasks-list");
+	finishedTaskList = $(".finished-tasks-list");
 	taskList.innerHTML = "";
 	finishedTaskList.innerHTML = "";
-	taskStore
-		.filter((task) => !task.completed && task.starred)
-		.forEach((task) => taskList.append(createDomLi(task)));
+	data.items
+		.filter((task) => !task.finished && task.starred)
+		.forEach((task) => taskList.append(createListItem(task)));
 
-	taskStore
-		.filter((task) => !task.completed && !task.starred)
-		.forEach((task) => taskList.append(createDomLi(task)));
+	data.items
+		.filter((task) => !task.finished && !task.starred)
+		.forEach((task) => taskList.append(createListItem(task)));
 
-	taskStore
-		.filter((task) => task.completed && task.starred)
-		.forEach((task) => finishedTaskList.append(createDomLi(task)));
+	data.items
+		.filter((task) => task.finished && task.starred)
+		.forEach((task) => finishedTaskList.append(createListItem(task)));
 
-	taskStore
-		.filter((task) => task.completed && !task.starred)
-		.forEach((task) => finishedTaskList.append(createDomLi(task)));
+	data.items
+		.filter((task) => task.finished && !task.starred)
+		.forEach((task) => finishedTaskList.append(createListItem(task)));
 }
 
-// function createTodo(it) {
-// 	var msg = it.msg;
-// 	var item = document.createElement("div");
-// 	var itemContent = document.createElement("div");
-// 	var itemDelete = document.createElement("button");
-// 	item.classList.add("list-item");
-// 	it.completed && item.classList.add(CL_COMPLETED);
-// 	itemContent.innerHTML = msg;
-// 	itemContent.className = "content";
-// 	itemDelete.type = "button";
-// 	itemDelete.innerHTML = " X ";
-// 	itemDelete.className = "delete";
-// 	item.appendChild(itemContent);
-// 	item.appendChild(itemDelete);
-// 	list.insertBefore(item, list.childNodes[0]);
 
-// 	// bind events
-// 	item.addEventListener("click", function () {
-// 		it.completed = !it.completed;
-// 		update();
-// 	});
-// 	itemDelete.addEventListener("click", function (event) {
-// 		data.items.splice(index, 1);
-// 		update();
-// 		event.stopPropagation();
-// 	});
-// }
-function createDomLi(taskObj) {
-	const newLi = document.createElement("li");
-	newLi.className = "task-list-item";
-	newLi.id = taskObj.taskId;
-	const newCheckButton = document.createElement("button");
-	newCheckButton.type = "button";
-	newCheckButton.className = "tasks-list-item__check-button";
-	if (!taskObj.completed) {
-		newCheckButton.innerHTML = "<i class='far fa-circle'></i>";
+function createListItem(taskObj) {
+	const item = document.createElement("li");
+	item.className = "task-list-item";
+	item.id = taskObj.taskId;
+	const checkButton = document.createElement("button");
+	checkButton.type = "button";
+	checkButton.className = "tasks-list-item_check-button";
+	if (!taskObj.finished) {
+		checkButton.innerHTML = "<i class='far fa-circle'></i>";
 	} else {
-		newCheckButton.innerHTML = "<i class='fas fa-check-circle'></i>";
+		checkButton.innerHTML = "<i class='fas fa-check-circle'></i>";
     }
 
-    newCheckButton.addEventListener("click", function (event) {
-        taskObj.completed = !taskObj.completed;
-        // const finishedContainer = document.querySelector(
-        //     ".finished-tasks-list__container"
+    checkButton.addEventListener("click", function (event) {
+        taskObj.finished = !taskObj.finished;
+        //Todo: 考虑有finish才hidden
+        // const finishedContainer = $(
+        //     ".finished-tasks-list_container"
         // );
         // finishedContainer.classList.remove("hidden");
 		update();
@@ -143,27 +99,27 @@ function createDomLi(taskObj) {
 	newTaskText.innerText = taskObj.msg;
     newTaskText.className = "task-text";
     
-	const newFavoriteButton = document.createElement("button");
-	newFavoriteButton.type = "button";
-	newFavoriteButton.className = "task-list-item__favorite-button";
+	const starButton = document.createElement("button");
+	starButton.type = "button";
+	starButton.className = "task-list-item_star-button";
 	if (!taskObj.starred) {
-		newFavoriteButton.innerHTML = "<i class='far fa-star gray'></i>";
+		starButton.innerHTML = "<i class='far fa-star gray'></i>";
 	} else {
-		newFavoriteButton.innerHTML = "<i class='fas fa-star'></i>";
+		starButton.innerHTML = "<i class='fas fa-star'></i>";
     }
 
-    newFavoriteButton.addEventListener("click", function (event) {
+    starButton.addEventListener("click", function (event) {
 		taskObj.starred = !taskObj.starred;
 		update();
 		event.stopPropagation();
     });
 
-	newLi.append(newCheckButton, newTaskText, newFavoriteButton);
-	return newLi;
+	item.append(checkButton, newTaskText, starButton);
+	return item;
 }
 
-function addTodo() {
-	var todo = $("todo");
+function addTask() {
+	var todo = $("#todo");
 	var msg = todo.value;
 	var data = model.data;
 	if (msg == "") {
@@ -174,7 +130,7 @@ function addTodo() {
 	data.items.push({
         taskId: `task${taskId}`,
 		msg: msg,
-		completed: false,
+		finished: false,
 		starred: false
     });
     taskId++;
