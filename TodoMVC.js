744f8b207ba5f16a03980fb41e8666969e3dd446
mooooon333 <742932097@qq.com>
2020-06-22 18:28:35 +0800

    注释无用代码“

--- TodoMVC.js
+++ TodoMVC.js
@@ -7,24 +7,23 @@ function $(id) {
 
 window.onload = function () {
 	model.init();
-	this.update();
+	
 	$("todo").addEventListener("keyup", function (event) {
 		if (event.keyCode != 13) return;
 		addTodo();
 	});
 	// 这里赋值const是没有用的，后边用不了
-	const taskList = document.querySelector(".tasks-list__list");
-	const finishedTaskList = document.querySelector(
-		".finished-tasks-list__list"
-	);
+	// const taskList = document.querySelector(".tasks-list__list");
+	// const finishedTaskList = document.querySelector(
+	// 	".finished-tasks-list__list"
+	// );
 	const finishedHeader = document.querySelector(".finished-header");
-	const finishedContainer = document.querySelector(
-		".finished-tasks-list__container"
-	);
 
-	taskList.addEventListener("click", clickTask);
+
+	// taskList.addEventListener("click", clickTask);
 	finishedHeader.addEventListener("click", toggleFinished);
-	finishedTaskList.addEventListener("click", clickTask);
+    // finishedTaskList.addEventListener("click", clickTask);
+    this.update();
 };
 function toggleFinished() {
     const finishedHeader = document.querySelector(".finished-header");
@@ -40,56 +39,33 @@ function toggleFinished() {
 			"finished-list-open",
 			"finished-list-closed"
 		);
-	}
+    }
+    const finishedTaskList = document.querySelector(
+		".finished-tasks-list__list"
+	);
 	finishedTaskList.classList.toggle("hidden");
 }
 
-function clickTask(e) {
-    // taskStore = model.data.items;
-	// const eventTarget = e.target;
-	// if (eventTarget.classList.contains("fa-star")) {
-	// 	const parentTask = eventTarget.parentNode.parentNode;
-	// 	taskStore = taskStore.map((task) => {
-	// 		if (task.taskId === parentTask.id) {
-	// 			return { ...task, starred: !task.starred };
-	// 		} else {
-	// 			return task;
-	// 		}
-	// 	});
-	// }
-	// if (eventTarget.classList.contains("fa-check-circle")) {
-	// 	finishedContainer.classList.remove("hidden");
-	// 	const parentTask = eventTarget.parentNode.parentNode;
-	// 	taskStore = taskStore.map((task) => {
-	// 		if (task.taskId === parentTask.id) {
-	// 			return { ...task, finished: !task.finished };
-	// 		} else {
-	// 			return task;
-	// 		}
-	// 	});
-    // }
-    // update();
-	// updateDom(taskStore);
-}
 
 function update() {
 	model.flush();
 	var data = model.data;
 
-	var list = $("list");
-	list.innerHTML = "";
-	for (var i = 0; i < data.items.length; ++i) {
-		(function (index) {
-			var it = data.items[index];
-			createTodo(it);
-		})(i);
-	}
+	// var list = $("list");
+	// list.innerHTML = "";
+	// for (var i = 0; i < data.items.length; ++i) {
+	// 	(function (index) {
+	// 		var it = data.items[index];
+	// 		createTodo(it);
+	// 	})(i);
+	// }
 
 	var activeCount = 0;
 	data.items.forEach(function (item) {
 		if (!item.completed) ++activeCount;
 	});
-	$("count").innerHTML = activeCount + " items left";
+    $("count").innerHTML = activeCount + " items left";
+    
 
 	var taskStore = data.items;
 	taskList = document.querySelector(".tasks-list__list");
@@ -113,33 +89,33 @@ function update() {
 		.forEach((task) => finishedTaskList.append(createDomLi(task)));
 }
 
-function createTodo(it) {
-	var msg = it.msg;
-	var item = document.createElement("div");
-	var itemContent = document.createElement("div");
-	var itemDelete = document.createElement("button");
-	item.classList.add("list-item");
-	it.completed && item.classList.add(CL_COMPLETED);
-	itemContent.innerHTML = msg;
-	itemContent.className = "content";
-	itemDelete.type = "button";
-	itemDelete.innerHTML = " X ";
-	itemDelete.className = "delete";
-	item.appendChild(itemContent);
-	item.appendChild(itemDelete);
-	list.insertBefore(item, list.childNodes[0]);
+// function createTodo(it) {
+// 	var msg = it.msg;
+// 	var item = document.createElement("div");
+// 	var itemContent = document.createElement("div");
+// 	var itemDelete = document.createElement("button");
+// 	item.classList.add("list-item");
+// 	it.completed && item.classList.add(CL_COMPLETED);
+// 	itemContent.innerHTML = msg;
+// 	itemContent.className = "content";
+// 	itemDelete.type = "button";
+// 	itemDelete.innerHTML = " X ";
+// 	itemDelete.className = "delete";
+// 	item.appendChild(itemContent);
+// 	item.appendChild(itemDelete);
+// 	list.insertBefore(item, list.childNodes[0]);
 
-	// bind events
-	item.addEventListener("click", function () {
-		it.completed = !it.completed;
-		update();
-	});
-	itemDelete.addEventListener("click", function (event) {
-		data.items.splice(index, 1);
-		update();
-		event.stopPropagation();
-	});
-}
+// 	// bind events
+// 	item.addEventListener("click", function () {
+// 		it.completed = !it.completed;
+// 		update();
+// 	});
+// 	itemDelete.addEventListener("click", function (event) {
+// 		data.items.splice(index, 1);
+// 		update();
+// 		event.stopPropagation();
+// 	});
+// }
 function createDomLi(taskObj) {
 	const newLi = document.createElement("li");
 	newLi.className = "task-list-item";
@@ -154,7 +130,11 @@ function createDomLi(taskObj) {
     }
 
     newCheckButton.addEventListener("click", function (event) {
-		taskObj.completed = !taskObj.completed;
+        taskObj.completed = !taskObj.completed;
+        // const finishedContainer = document.querySelector(
+        //     ".finished-tasks-list__container"
+        // );
+        // finishedContainer.classList.remove("hidden");
 		update();
 		event.stopPropagation();
     });
